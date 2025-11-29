d34176722b2a3f154357641517f8d221229da15f
KarbonST <mikhailbykadorov@mail.ru>
2025-11-30 01:24:02 +0300

    feat(main): added Basket class

--- src/Basket.java
+++ src/Basket.java
@@ -3,8 +3,37 @@ import java.util.List;
 
 public class Basket<F extends Fruit>{
 
-    private List<? extends Fruit> array;
-    public Basket(List<? extends Fruit> array) {
-        this.array = array;
+    private final List<F> array;
+
+    public Basket(){
+        array = new ArrayList<>();
+    }
+
+    public double getWeight(){
+        double sum = 0;
+
+        for (F fruit : array){
+            sum += fruit.getWeight();
+        }
+        return sum;
+    }
+
+    public void add (F fruit){
+        array.add(fruit);
+    }
+
+    public int compare(Basket<? extends Fruit> other){
+        return Double.compare(this.getWeight(), other.getWeight());
+    }
+
+    public void transferTo(Basket<? super F> dst){
+        dst.clear();
+        for (F fruit : array){
+            dst.add(fruit);
+        }
+    }
+
+    public void clear(){
+        array.clear();
     }
 }
